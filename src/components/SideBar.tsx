11aeb998d6696a74797c5302f3677f619a59e500
gledson-ss <github@gledson.dev>
2021-09-01 21:30:51 -0300

    finished challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import { api } from '../services/api';
+import { Button } from '../components/Button';
+import { useEffect, useState } from 'react';
+import '../styles/sidebar.scss';
+
+interface propsSideBar {
+  selectedGenreId: number
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>
+}
+
+export function SideBar({ selectedGenreId, setSelectedGenreId }: propsSideBar) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar" style={{ border: '1px solid red' }}>
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
